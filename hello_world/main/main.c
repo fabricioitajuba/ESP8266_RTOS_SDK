4f0aa3a5bedd5c64e136de9cb8119bd48335ced1
Fabrício de Lima Ribeiro <31161759+fabricioitajuba@users.noreply.github.com>
2023-12-27 10:41:09 -0300

    Hello World

---
+++ hello_world/main/main.c
@@ -0,0 +1,17 @@
+/*
+ Programa Teste
+ Autor Fabrício Ribeiro
+ Data: 27/12/2023
+*/
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+
+void app_main()
+{
+    printf("Hello world!\n");
+
+    while(true){
+
+    }
+}
